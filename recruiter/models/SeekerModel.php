a8c76b19ff70bd98f8a83faf130d5305c0d6d8bb
MahirMsiam <mahirmahmudsiam@gmail.con>
2026-05-18 10:52:22 +0600

    Update SeekerModel.php

--- recruiter/models/SeekerModel.php
+++ recruiter/models/SeekerModel.php
@@ -68,7 +68,7 @@ class SeekerModel {
                     sp.headline, sp.summary, sp.skills, sp.years_experience, 
                     sp.education_level, sp.expected_salary, sp.preferred_location, sp.resume_path
              FROM users u
-             LEFT JOIN seeker_profiles sp ON u.id = sp.user_id
+             INNER JOIN seeker_profiles sp ON u.id = sp.user_id
              WHERE u.id = ? AND u.role = 'seeker'"
         );
         $stmt->bind_param("i", $seeker_id);
